cc5cf7265d9305568a40f443c64e4a755cfd2e0b
eren <dengdengd1222@mail.com>
2026-03-19 00:17:15 +0800

    client_rpc

--- include/agent_rpc/rpc_framework.h
+++ include/agent_rpc/rpc_framework.h
@@ -2,7 +2,7 @@
  * @Author: eren dengdengd1222@mail.com
  * @Date: 2026-03-14 10:36:25
  * @LastEditors: eren dengdengd1222@mail.com
- * @LastEditTime: 2026-03-17 14:48:27
+ * @LastEditTime: 2026-03-18 18:54:11
  * @FilePath: /my_agent_communication/include/agent_rpc/rpc_framework.h
  * @Description: 
  * 
@@ -17,8 +17,11 @@
 #include <atomic>
 #include <grpcpp/health_check_service_interface.h>
 #include "logger.h"
+#include "metric.h"
+#include "proto/agent_service.grpc.pb.h"
 namespace agent_rpc {
-
+// 前向声明
+class Metrics;
 // 配置结构
 struct RpcConfig {
     std::string server_address = "0.0.0.0:50051";
@@ -46,6 +49,47 @@ struct ServiceEndpoint {
     
 };
 
+// 消息队列
+template<typename T> 
+class MessageQueue {
+public:
+    void push(const T& item) {
+        std::lock_guard<std::mutex> locker(mutex_);
+        queue_.push(item);
+        condition_.notify_one();
+    }
+
+    bool try_pop(T& item, std::chrono::milliseconds timeout = std::chrono::milliseconds(0)) {
+        std::unique_lock<std::mutex> locker(mutex_);
+        if (condition_.wait_for(locker, timeout, [this] { return !queue_.empty(); })) {
+            item = queue_.front();
+            queue_.pop();
+            return true;
+        }
+        return false;
+    }
+
+    size_t size() const {
+        std::lock_guard<std::mutex> lock(mutex_);
+        return queue_.size();
+    }
+
+    bool empty() const {
+        std::lock_guard<std::mutex> locker(mutex_);
+        return queue_.empty();
+    }
+
+private:
+    mutable std::mutex mutex_;
+    std::queue<T> queue_;
+    std::condition_variable condition_;
+};
+
+// 回调函数类型定义
+using MessageHandler = std::function<void(const agent_communication::Message &)>;
+
+using ErrorHandler = std::function<void(const std::string&, int)>;
+using HealthCheckHandler = std::function<bool()>;
 } // namespace agent_rpc
 
 
---
+++ src/rpc_server.cpp
@@ -0,0 +1,13 @@
+/*
+ * @Author: eren dengdengd1222@mail.com
+ * @Date: 2026-03-18 20:51:26
+ * @LastEditors: eren dengdengd1222@mail.com
+ * @LastEditTime: 2026-03-18 20:54:51
+ * @FilePath: /my_agent_communication/src/rpc_server.cpp
+ * @Description
+ * 
+ */
+#include "rpc_server.h"
+
+// AgentCommunicationServiceImpl 实现
+AgentCommunicationServiceImpl::
